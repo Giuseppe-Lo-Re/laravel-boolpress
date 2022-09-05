995acec5175e403b14fa541426534eee4cd297bd
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-09-05 15:51:35 +0200

    foreach for card done, title populated

--- resources/views/admin/Posts/index.blade.php
+++ resources/views/admin/Posts/index.blade.php
@@ -1,3 +1,24 @@
-<h1>
-    index posts
-</h1>
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>Lista Post</h1>
+
+    <div class="row row-cols-2 gy-5">
+        @foreach ($posts as $post)
+            
+            {{-- Single Card --}}
+            <div class="col">
+                <div class="card mt-5">
+                    {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                    <div class="card-body">
+                    <h5 class="card-title">{{ $post->title }}</h5>
+                    {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
+                    {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
+                    </div>
+                </div>
+            </div>
+            {{-- End Single Card --}}
+        @endforeach
+
+    </div>
+@endsection     
