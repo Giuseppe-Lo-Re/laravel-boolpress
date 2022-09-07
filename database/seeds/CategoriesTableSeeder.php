f4769a28eb75f5b1ee5e6ffbfb434b2ab0ca66f9
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-09-07 14:51:33 +0200

    model and seeder done

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Category;
+use illuminate\Support\Str;
 class CategoriesTableSeeder extends Seeder
 {
     /**
@@ -11,6 +12,21 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // creo le categorie
+        $categories = [
+            'Attualità',
+            'Cronaca',
+            'Politica',
+            'Sport',
+            'Viaggi'
+        ];
+        // creo una nuova riga nel database per ogni elemento di $categories 
+        foreach($categories as $category_name) {
+            $new_category = new Category();
+            $new_category->name = $category_name;
+            $new_category->slug = Str::slug($category_name, '-');
+            $new_category->save();
+
+        }
     }
 }
