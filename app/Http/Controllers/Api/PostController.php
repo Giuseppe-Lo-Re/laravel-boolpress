5fbccc3e31460e3c759af76c6c441ef3fffedafd
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-09-12 13:53:13 +0200

    Api/PostController + route created

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index() {
+
+        // Richiamo tutti i post
+        $posts = Post::all();
+
+        $data = [
+            'success' => true,
+            'results' => $posts
+        ];
+    
+        // Ritorno un file json anzichè una view
+        return response()->json($data);
+    }   
+
+}
