a882fae38dfcf52bf27374f55536b1fe4a8b7799
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-09-13 17:23:41 +0200

    adjusted spaces and syntax

--- resources/js/components/HeaderPage.vue
+++ resources/js/components/HeaderPage.vue
@@ -1,23 +1,19 @@
 <template>
   <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      
-      
-      
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div  v-for="link, index in menuLinks" :key="index" class="navbar-nav">
           <router-link :to="{name: link.name}" class="nav-item nav-link">{{link.label}}</router-link>
-          
         </div>
       </div>
     </nav>
   </header>
 </template>
 
-<script> //
+<script> 
 export default {
    name: 'HeaderPage',
    data() {
