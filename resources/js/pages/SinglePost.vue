165da7d128df0e7d5a6273610abc65ca261a6d42
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-09-14 14:47:55 +0200

    SinglePost page created

---
+++ resources/js/pages/SinglePost.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="container">
+    <h1>
+        sono la pagina del dettaglio
+    </h1>
+
+    {{ $route.params.slug }}
+</div>
+</template>
+
+<script>
+export default {
+    name: 'SinglePost'
+}
+</script>
